f6dd4b9b52d116804cfcdabef777edddc7544264
yeomdogyeong <ehrud1031@naver.com>
2026-03-09 11:40:05 +0900

    Fix: change basic route

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/QA_Test/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
